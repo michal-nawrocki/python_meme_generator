10f630e3324c255c89d1ddc7c30b12d85c6031b9
michal-nawrocki <michal.m.nawrocki@gmail.com>
2020-08-02 23:36:45 +0100

    2020.08.02 - Acomic_sans font

--- meme_generator/font.py
+++ meme_generator/font.py
@@ -2,7 +2,8 @@ from enum import Enum
 
 
 class Font(Enum):
-    IMPACT = "fonts/impact.ttf"
+    IMPACT = "../fonts/impact.ttf"
+    COMIC_SANS = "../fonts/comic_sans.ttf"
 
     def __str__(self):
         return str(self.value)
